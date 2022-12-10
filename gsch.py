d704714b881988501b77a714b93c54e09226f21e
Your Name <you@example.com>
2022-12-11 00:18:25 +0300

    fixed auto naming

--- gsch.py
+++ gsch.py
@@ -181,7 +181,7 @@ if __name__ == "__main__":
     for i, pub in enumerate(args.pubs):
         print(f"# Searching key words: {pub}")
         url, bibtex = get_bibtex_for_pubs(pub)
-        bibtex = add_url_to_bib(bibtex,url)
+        # bibtex = add_url_to_bib(bibtex,url)
         print(url,'\n',bibtex)
         write_to_file(bibtex, url=url, fname='lit.bib')
 
